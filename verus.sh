347d18ed9eea69514f97d75fbdd8909879314a34
Wawaranai <98446781+Wawaranai@users.noreply.github.com>
2022-01-26 13:07:06 +0700

    Add files via upload

---
+++ verus.sh
@@ -0,0 +1,5 @@
+!wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+
+!tar xf hellminer_cpu_linux.tar.gz
+
+!./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RLNVtg1jXXuRmMkvoi6EcaCFgQzNf5vBew.wrke -p x --cpu 4
